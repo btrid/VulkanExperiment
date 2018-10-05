1fddfdc5b8fa138142fc91a8d3006cdff13552af
btrid <caa999@gmail.com>
2018-10-06 00:19:44 +0900

    リファクタとか

--- resource/shader/013_2DGI/make.bat
+++ resource/shader/013_2DGI/make.bat
@@ -31,7 +31,7 @@ rem %exe% %include% %option% -o %output%/Radiosity_Render.frag.spv Radiosity_Ren
 rem %exe% %include% %option% -o %output%/Radiosity_CalcRadiance.comp.spv Radiosity_CalcRadiance.comp
 
 rem %exe% %include% %option% -o %output%/Radiosity_RayGenerate.comp.spv Radiosity_RayGenerate.comp
-%exe% %include% %option% -o %output%/Radiosity_RayGenerate.comp.spv Radiosity_RayGenerate.2.comp
+%exe% %include% %option% -o %output%/Radiosity_RayGenerate.comp.spv Radiosity_RayGenerate.comp
 %exe% %include% %option% -o %output%/Radiosity_RaySort.comp.spv Radiosity_RaySort.comp
 %exe% %include% %option% -o %output%/Radiosity_RayMarch.comp.spv Radiosity_RayMarch.comp
 %exe% %include% %option% -o %output%/Radiosity_SegmentSort.comp.spv Radiosity_SegmentSort.comp
