e27bd67bc8b1927fe3f8ecb6b6e6015befe4a508
kaisya <caa999@gmail.com>
2020-03-06 18:28:51 +0900

    見た目修正

--- resource/shader/015_ShaderToy/Sky.glsl
+++ resource/shader/015_ShaderToy/Sky.glsl
@@ -20,9 +20,9 @@ layout(set=USE_Sky, binding=11, rgba16) uniform image2D i_render_map;
 // 雲の光の吸収量
 #define ABSORPTION		0.15
 
-const float u_plant_radius = 1000.;
+const float u_plant_radius = 10000.;
 const vec4 u_planet = vec4(0., -u_plant_radius, 0, u_plant_radius);
-const vec4 u_cloud_inner = vec4(u_planet.xyz, u_planet.w*1.05);
+const vec4 u_cloud_inner = vec4(u_planet.xyz, u_planet.w+200);
 const vec4 u_cloud_outer = u_cloud_inner + vec4(0., 0., 0, 16.);
 const float u_cloud_area_inv = 1. / (u_cloud_outer.w - u_cloud_inner.w);
 const float u_mapping = 1./u_cloud_outer.w;
