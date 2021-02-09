7a3244b31245011a1e2e4a32a34e594bea88d608
btrid <caa999@gmail.com>
2021-02-09 20:10:58 +0900

    新しいプロジェクト追加

--- include/btrlib/Context.h
+++ include/btrlib/Context.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <filesystem>
 #include <vector>
 #include <btrlib/Define.h>
@@ -30,34 +31,6 @@ struct Context
 //	uint32_t getGPUFrame()const { return m_window->getSwapchain().m_backbuffer_index; }
 	uint32_t getCPUFrame()const { return sGlobal::Order().getWorkerFrame(); }
 	uint32_t getGPUFrame()const { return sGlobal::Order().getRenderFrame(); }
-
-	vk::UniqueShaderModule loadShaderUnique(const std::string& filename)
-	{
-		std::filesystem::path p(filename);
-		std::ifstream file(p, std::ios_base::ate | std::ios::binary);
-		assert(file.is_open());
-
-		size_t file_size = (size_t)file.tellg();
-		file.seekg(0);
-
-		std::vector<char> buffer(file_size);
-		file.read(buffer.data(), buffer.size());
-
-		vk::ShaderModuleCreateInfo shaderInfo = vk::ShaderModuleCreateInfo()
-			.setPCode(reinterpret_cast<const uint32_t*>(buffer.data()))
-			.setCodeSize(buffer.size());
-
-		auto shader_module = m_device.createShaderModuleUnique(shaderInfo);
-
-#if USE_DEBUG_REPORT
-		vk::DebugUtilsObjectNameInfoEXT name_info;
-		name_info.pObjectName = filename.c_str();
-		name_info.objectType = vk::ObjectType::eShaderModule;
-		name_info.objectHandle = reinterpret_cast<uint64_t &>(shader_module);
-		m_device.setDebugUtilsObjectNameEXT(name_info);
-#endif
-		return shader_module;
-	}
 };
 }
 
---
+++ source/017_RayTracing/017_main.cpp
@@ -0,0 +1,75 @@
+﻿#include <btrlib/Define.h>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <utility>
+#include <array>
+#include <unordered_set>
+#include <vector>
+#include <functional>
+#include <thread>
+#include <future>
+#include <chrono>
+#include <memory>
+#include <filesystem>
+#include <btrlib/cWindow.h>
+#include <btrlib/cInput.h>
+#include <btrlib/cCamera.h>
+#include <btrlib/sGlobal.h>
+#include <btrlib/GPU.h>
+#include <btrlib/cStopWatch.h>
+#include <btrlib/AllocatedMemory.h>
+
+#include <applib/App.h>
+#include <applib/AppPipeline.h>
+#include <btrlib/Context.h>
+
+#pragma comment(lib, "btrlib.lib")
+#pragma comment(lib, "applib.lib")
+#pragma comment(lib, "vulkan-1.lib")
+#pragma comment(lib, "imgui.lib")
+
+
+int main()
+{
+	auto camera = cCamera::sCamera::Order().create();
+	camera->getData().m_position = vec3(0.f, 0.f, 1.f);
+	camera->getData().m_target = vec3(0.f, 0.f, 0.f);
+	camera->getData().m_up = vec3(0.f, -1.f, 0.f);
+	camera->getData().m_width = 640;
+	camera->getData().m_height = 480;
+	camera->getData().m_far = 5000.f;
+	camera->getData().m_near = 0.01f;
+
+	app::AppDescriptor app_desc;
+	app_desc.m_window_size = uvec2(640, 480);
+	app::App app(app_desc);
+
+	auto context = app.m_context;
+
+	app.setup();
+
+	auto render_target = app.m_window->getFrontBuffer();
+	ClearPipeline clear_render_target(context, render_target);
+	PresentPipeline present_pipeline(context, render_target, context->m_window->getSwapchain());
+
+	while (true)
+	{
+		cStopWatch time;
+
+		app.preUpdate();
+		{
+			std::vector<vk::CommandBuffer> cmds =
+			{
+				clear_render_target.execute(),
+				present_pipeline.execute(),
+			};
+			app.submit(std::move(cmds));
+		}
+		app.postUpdate();
+		printf("%6.4fms\n", time.getElapsedTimeAsMilliSeconds());
+	}
+
+	return 0;
+}
+
