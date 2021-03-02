04819e43a82df47658f883e175be861c8515b6f1
btrid <caa999@gmail.com>
2021-03-02 12:27:06 +0900

    整理

--- resource/shader/017_RayTracing/Voxel.glsl
+++ resource/shader/017_RayTracing/Voxel.glsl
@@ -24,12 +24,6 @@ struct LeafNode
 	uint32_t albedo;
 };
 
-struct LeafData
-{
-	uvec2 bitmask;
-	u16vec3 pos;
-	uint leaf_index;
-};
 
 layout(set=USE_Voxel,binding=0, std140) uniform V0 {VoxelInfo u_info; };
 layout(set=USE_Voxel,binding=1, scalar) buffer V1 { int b_hashmap[]; };
@@ -37,63 +31,9 @@ layout(set=USE_Voxel,binding=2, scalar) buffer V2 { ivec4 b_interior_counter[]; 
 layout(set=USE_Voxel,binding=3, scalar) buffer V3 { int b_leaf_counter; };
 layout(set=USE_Voxel,binding=4, scalar) buffer V4 { InteriorNode b_interior[]; };
 layout(set=USE_Voxel,binding=5, scalar) buffer V5 { LeafNode b_leaf[]; };
-layout(set=USE_Voxel,binding=6, scalar) buffer V6 { ivec4 b_leaf_data_counter; };
-layout(set=USE_Voxel,binding=7, scalar) buffer V7 { LeafData b_leaf_data[]; };
-
-//layout(set=USE_Voxel,binding=10, scalar) buffer V10 { ivec4 b_fragment_counter; };
-//layout(set=USE_Voxel,binding=11, scalar) buffer V11 { i16vec4 b_fragment_data[]; };
 
 layout(set=USE_Voxel,binding=10, std140) uniform V10 {mat4 u_voxelize_pvmat[3]; };
 #endif
-
-
-float sdBox( vec3 p, vec3 b )
-{
-	vec3 d = abs(p) - b;
-	return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
-}
-float sdSphere( vec3 p, float s )
-{
-  return length( p ) - s;
-}
-
-float sdf(in vec3 p)
-{
-	float d = 9999999.;
-	{
-//		d = min(d, sdSphere(p-vec3(1000., 250., 1000.), 1000.));
-
-		d = min(d, sdBox(p-vec3(500., 200., 700.), vec3(300.)));
-		d = min(d, sdSphere(p-vec3(1200., 190., 1055.), 200.));
-	}
-	return d;
-}
-
-bool map(in vec3 p)
-{
-	p-=0.5;
-	float d = sdf(p);
-	for(int z = 0; z < 2; z++)
-	for(int y = 0; y < 2; y++)
-	for(int x = 0; x < 2; x++)
-	{
-		if(z==0 && y==0 && x==0){continue;}
-		if(sign(d) != sign(sdf(p + vec3(x, y, z))))
-		{
-			return true;
-		}
-	}
-	return false;
-}
-
-vec3 normal(in vec3 p)
-{
-	p-=0.5;
-	float x = sdf(p-vec3(0.1, 0.0, 0.0)) - sdf(p+vec3(0.1, 0.0, 0.0));
-	float y = sdf(p-vec3(0.0, 0.1, 0.0)) - sdf(p+vec3(0.0, 0.1, 0.0));
-	float z = sdf(p-vec3(0.0, 0.0, 0.1)) - sdf(p+vec3(0.0, 0.0, 0.1));
-	return normalize(vec3(x, y, z));
-}
 
 ivec3 ToTopIndex(in ivec3 p){ return p >> 4; }
 ivec3 ToMidIndex(in ivec3 p){ return p >> 2; }
